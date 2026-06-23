2f3cc5079e727dbbc188ca72b96cdcd0c5df819c
Apurba <apurbakar1111@gmail.com>
2026-06-23 12:16:58 +0530

    Update axios base URL to use VITE_API_URL

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
-const BASE_URL = import.meta.env.MODE === "development"? "http://localhost:3000/api/v1" : "/api/v1";
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
