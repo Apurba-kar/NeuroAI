d1ea73ef1e60dd8ebd09a8c9692eda6dfe0f0b7f
Apurba <apurbakar1111@gmail.com>
2026-06-23 13:15:54 +0530

    Refactor cookie settings in createSendToken function
    Refactor createSendToken function to use cookieOptions for setting cookie properties.

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -11,16 +11,20 @@ const SignToken = (id) =>
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
-const createSendToken = (user, statusCode, req, res) => {
-  const token = SignToken(user._id);
+const cookieOptions = {
+  expires: new Date(
+    Date.now() +
+      process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+  ),
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite:
+    process.env.NODE_ENV === "production"
+      ? "none"
+      : "lax",
+};
 
-  res.cookie("jwt", token, {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
-  });
+res.cookie("jwt", token, cookieOptions);
 
   //Remove the password from the output
   user.password = undefined;
